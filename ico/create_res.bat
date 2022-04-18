0a05be3caa6ad7fb574602361fec6a4dab9f11d8
Aysi-E <773917760@qq.com>
2022-04-18 21:15:47 +0800

    20220418

--- ico/create_res.bat
+++ ico/create_res.bat
@@ -1,5 +1,5 @@
-;皆为unsigned char指针处理
-;例: extern unsigned char _binary_input_txt_start[];
+::皆为unsigned char指针处理
+::例: extern unsigned char _binary_input_txt_start[];
  ld -r -b binary -o res.o ff.gif
 objdump -x res.o
 @pause
